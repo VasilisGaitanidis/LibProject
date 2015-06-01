e614e86aee0edba95522ff3f008d0865af387ffc
VasilisGaitanidis <vasilis.gaitanidis@yahoo.gr>
2015-06-01 23:47:02 +0300

    Hibernate_Commit_v02_03
    To do: ComboBox SearchForm
    JOptionPane (optional) none borrows

--- src/MemberBookTableModel.java
+++ src/MemberBookTableModel.java
@@ -23,13 +23,13 @@ public class MemberBookTableModel extends AbstractTableModel {
 	@Override
 	//Sets the number of Columns
 	public int getColumnCount() {
-		return 6;				//Check for number of columns
+		return 7;				//Check for number of columns
 	}
 
 	@Override
 	//Column Title Names
 	public String getColumnName(int column) {
-		String[] columnNames=new String[]{"Κωδικός Δανεισμού", "Ημέρα Δανεισμού", "Ημέρα Παράδοσης", "Ημέρα Επιστροφής", "ISBN", "Αριθμός Μητρώου"};
+		String[] columnNames=new String[]{"Κωδικός Δανεισμού", "Ημέρα Δανεισμού", "Ημέρα Παράδοσης", "Ημέρα Επιστροφής", "ISBN","Τίτλος Βιβλίου", "Αριθμός Μητρώου"};
 		return columnNames[column];
 	}
 
@@ -38,7 +38,7 @@ public class MemberBookTableModel extends AbstractTableModel {
 	public Object getValueAt(int rowIndex, int columnIndex) {
 		MemberBook mb = memberBooks.get(rowIndex);
 		Object[] values=new Object[]{
-				mb.getBorrowID(),mb.getBorrowDay(),mb.getReturnDay(),mb.getDeliveryDay(),mb.getBook().getiSBN(),mb.getMember().getiD()
+				mb.getBorrowID(), mb.getBorrowDay(), mb.getReturnDay(), mb.getDeliveryDay(), mb.getBook().getiSBN(), mb.getBook().getBookName(), mb.getMember().getiD()
 		};
 		return values[columnIndex];
 	}        
